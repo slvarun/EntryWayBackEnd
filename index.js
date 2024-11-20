2ec665e687f62ca00fdf0a153ffd3baca278762b
slvarun <varun.merugu@yahoo.in>
2024-11-20 18:45:46 +0530

    Initial commit-2

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose"
+import monumentsRoute from "./routes/monuments.js"
+import userRoute from "./routes/users.js"
+import authRoute from "./routes/auth.js"
+import bookingRoute from "./routes/bookingorder.js"
+import AdminRoute from "./routes/admin.js"
+import cookieParser from "cookie-parser"
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+
+mongoose.set('strictQuery', true);
+const connect = async()=> {
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log(("connected to db"))
+    }catch(error){
+        console.log("Not connected")
+    }
+}
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected")
+})
+
+
+app.use(cors());
+
+
+app.use(cookieParser())
+                                //  default middlewares
+app.use(express.json())                 
+
+
+
+
+
+app.use("/monuments",monumentsRoute);
+app.use("/users", userRoute)
+                                                    //  using other files as library
+app.use("/auth", authRoute)
+app.use("/bookingorder", bookingRoute)
+app.use("/admin", AdminRoute)
+
+
+
+
+
+app.use((err, req, res, next)=>{
+    const errorStatus = err.status ||500
+    const errormsg = err.message ||"Something went wrong"
+
+    return res.status(errorStatus).json({
+        success : false,
+        status:errorStatus,
+        message: errormsg,
+        stack : err.stack,
+    })
+})
+
+app.listen(process.env.PORT || 5000, ()=>{
+    connect();
+    console.log("Listening to 5000");
+})
---
+++ routes/monuments.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {countByCity, createmon, deletemon, getallmon, getmon, updatemon } from "../controllers/monuments.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+
+
+
+const router = express.Router();
+
+
+router.get("/", getallmon)
+router.get("/countByCity", countByCity)
+
+                                    
+router.post("/create_mon", createmon)
+
+
+router.put("/:id",verifyAdmin, updatemon)
+
+
+router.delete("/:id",verifyAdmin,deletemon)
+
+
+
+router.get("/find/:id", getmon)
+
+
+
+
+export default router
